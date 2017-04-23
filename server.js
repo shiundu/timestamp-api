aa7ee4401c51e308840062ce23ba08ae0f5849b4
Ben Shiundu <benshiundu@gmail.com>
2017-04-23 17:02:05 +0000

    all code

--- server.js
+++ server.js
@@ -1,10 +1,67 @@
 var express = require('express')
 var app = express()
+var fs = require('fs');
+var port = process.env.PORT || 80;
 
 app.get('/', function(req, res){
-    res.send('Hello World')
+
+    fs.readFile('./readme.html', function (err, html) {
+        if (err) {
+            throw err; 
+        }  
+        res.writeHeader(200, {"Content-Type": "text/html"});  
+        res.write(html);  
+        res.end();  
+        
+      //res.send(express.static( 'readme.js' || (__dirname, 'public')))
+    })
+})    
+
+app.get('/:date', function(req, res){
+    
+    var date = req.params.date
+    
+    if(isNaN(date)){
+        if((new Date(date)).getTime()){
+            var normal = formatDate(new Date(date))
+            var unixtime = (new Date(date)).getTime()
+        }
+        else {
+            var normal = null;
+            var unixtime = null;
+        }
+    }
+    else if(!isNaN(date)){
+        var normal = formatDate(new Date(+date))
+        var unixtime = (new Date(+date)).getTime()
+    }
+    else {
+      var normal = null;
+      var unixtime = null;
+    }
+    function formatDate(date) {
+      var monthNames = [
+        "January", "February", "March",
+        "April", "May", "June", "July",
+        "August", "September", "October",
+        "November", "December"
+      ];
+    
+      var day = date.getDate();
+      var monthIndex = date.getMonth();
+      var year = date.getFullYear();
+    
+      return monthNames[monthIndex] + ' ' + day + ', ' + year;
+    }
+    
+    var send = {
+        unix: unixtime,
+        natural: normal
+    }
+    
+    res.send(send)
 })
 
-app.listen(8080, function(){
-    console.log("Timestamp api listening on port 8080")
+app.listen(port, function(){
+    console.log("Timestamp api started")
 })
